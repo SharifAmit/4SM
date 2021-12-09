f4840524ca28ae24fa7db9bdc3fb000c9dbb20a7
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2021-12-09 08:54:23 -0800

    Create windows.sh

---
+++ windows.sh
@@ -0,0 +1,10 @@
+conda install tensorflow-gpu=2.0.0
+pip install tensorflow-estimator==2.0.0
+conda install keras=2.3.1
+pip install streamlit
+pip install streamlit-aggrid
+pip install opencv-python
+pip uninstall scikit-learn
+pip install scikit-learn==0.20.4
+pip install pycm
+conda deactivate streamlit-calciumgan
