a86d4f3a3c5a988cc7bdb4c5d73e8eba55626bee
SharifAmit <sharif.amit19@gmail.com>
2021-12-09 10:14:46 -0800

    bash

--- windows.sh
+++ windows.sh
@@ -1,10 +1,9 @@
-conda install tensorflow-gpu=2.0.0
+conda install -y tensorflow-gpu=2.0.0
 pip install tensorflow-estimator==2.0.0
-conda install keras=2.3.1
+conda install -y keras=2.3.1
 pip install streamlit
 pip install streamlit-aggrid
 pip install opencv-python
-pip uninstall scikit-learn
+pip uninstall -y scikit-learn
 pip install scikit-learn==0.20.4
 pip install pycm
-conda deactivate streamlit-calciumgan
